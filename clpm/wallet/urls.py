90769580505edf8cf14fa949bd7b76d58aeec087
Thomas Gredin <t.gredin@gmx.fr>
2019-05-06 19:37:16 +0200

    Work on account management, account details page

--- clpm/wallet/urls.py
+++ clpm/wallet/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('reload/', views.charge, name='reload'),
-    path('shop/', views.spend, name='shop'),
+    path('reload/<int:id>', views.reload, name='reload'),
+    path('shop/<int:id>', views.shop, name='shop'),
+    path('account/<int:id>', views.account_index, name='account'),
 ]
--- clpm/wallet/views.py
+++ clpm/wallet/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Account
 
 # index view
 def index(request):
+    request.session['current_account_id'] = -1
     context = {
         'title': "Compte | Accueil",
         'accounts': Account.objects.all()
@@ -12,15 +13,77 @@ def index(request):
     return render(request, 'wallet/index.html', context)
 
 
-def charge(request):
+# Account management main view
+def account_index(request, id = -1):
+    account = getAccount(request, id)
+
+    # Check if account has been found
+    if(account == None):
+        redirect('index')
+
+    # Set session
+    request.session['current_account_id'] = account.id
+
+    # Create context
+    context = {
+        'title': "Compte | {}".format(account.name),
+        'current_account': account,
+    }
+
+    # Return the view
+    return render(request, 'wallet/account.html', context)
+
+
+def reload(request, id):
+    account = getAccount(request, id)
+
+    # Check if account has been found
+    if(account == None):
+        redirect('index')
+
+    # Set session
+    request.session['current_account_id'] = account.id
+
     context = {
         'title': "Compte | Rechargement",
+        'current_account': account,
     }
-    return render(request, 'wallet/charge.html', context)
+    return render(request, 'wallet/reload.html', context)
 
 
-def spend(request):
+def shop(request, id):
+    account = getAccount(request, id)
+
+    # Check if account has been found
+    if(account == None):
+        redirect('index')
+
+    # Set session
+    request.session['current_account_id'] = account.id
+
     context = {
         'title': "Compte | Boutique",
+        'current_account': account,
     }
-    return render(request, 'wallet/spend.html', context)
+    return render(request, 'wallet/shop.html', context)
+
+
+'''
+Utility function to get account from id, 
+return None if no account found
+'''
+def getAccount(request, id):
+    if(id == -1):
+        # Check if account exists in session
+        id = request.session['current_account_id']
+        if(id == -1):
+            # redirect to index
+            return None
+
+    account = Account.objects.get(id=id)
+
+    # Check if account has been found
+    if(account == None):
+        return None
+
+    return account
