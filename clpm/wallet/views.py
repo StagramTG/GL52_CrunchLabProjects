cf41f2b3deb21f9880767f453d74ea494c28c586
Thomas Gredin <t.gredin@gmx.fr>
2019-05-06 14:55:39 +0200

    Dispatch html page parts in differents files

--- clpm/wallet/views.py
+++ clpm/wallet/views.py
@@ -6,12 +6,21 @@ from .models import Account
 # index view
 def index(request):
     context = {
+        'title': "Compte | Accueil",
         'accounts': Account.objects.all()
     }
     return render(request, 'wallet/default.html', context)
 
+
 def charge(request):
-	return render(request, 'wallet/charge.html')
+    context = {
+        'title': "Compte | Rechargement",
+    }
+    return render(request, 'wallet/charge.html', context)
+
 
 def spend(request):
-	return render(request, 'wallet/spend.html')
+    context = {
+        'title': "Compte | Boutique",
+    }
+    return render(request, 'wallet/spend.html', context)
