4b698cfacc1576279be620370b941f1b1da40d6c
Thomas Gredin <t.gredin@gmx.fr>
2019-06-03 18:00:25 +0200

    Setup api-auth urls for authentication

--- clpm/clpm/urls.py
+++ clpm/clpm/urls.py
@@ -24,5 +24,6 @@ router.register('test', TestViews)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
