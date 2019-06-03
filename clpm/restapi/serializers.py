422b3e9f9214a2224076032a375d27e503bcf7d5
Thomas Gredin <t.gredin@gmx.fr>
2019-06-03 09:45:02 +0200

    Add account serializer + end of session

--- clpm/restapi/serializers.py
+++ clpm/restapi/serializers.py
@@ -1,3 +1,7 @@
 from rest_framework import serializers
 from . import models
 
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Account
+        fields = ('id', 'balance', 'name')
