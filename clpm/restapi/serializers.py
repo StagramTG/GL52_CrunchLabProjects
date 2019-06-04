1363958800ad4ea712b8e613e3df3994cc9750af
Thomas Gredin <t.gredin@gmx.fr>
2019-06-04 10:27:40 +0200

    Create serializers for all Models

--- clpm/restapi/serializers.py
+++ clpm/restapi/serializers.py
@@ -5,3 +5,60 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Account
         fields = ('id', 'balance', 'name')
+
+
+class AccountTransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AccountTransaction
+        fields = ('id', 'amount', 'created_at', 'account_id')
+
+
+class AccountReloadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AccountReload
+        fields = ('id', 'amount', 'created_at', 'account_id')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = (
+            'id', 'email', 'first_name', 'last_name', 'location', 
+            'phone', 'account_id', 'is_admin'
+        )
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Project
+        fields = (
+            'id', 'name', 'description', 'account_id', 'user_id', 
+            'created_at'
+        )
+
+
+class ProjectRolesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProjectRoles
+        fields = ('id', 'name')
+
+
+class UserProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProject
+        fields = ('id', 'user_id', 'project_id')
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Task
+        fields = (
+            'id', 'name', 'project_id', 'created_at', 'started_at', 
+            'ended_at'
+        )
+
+
+class TaskUserAssignementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.TaskUserAssignement
+        fields = ('id', 'user_id', 'task_id')
