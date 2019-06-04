817dbae9d71588a5a04e7b64100b6c6b82433283
Thomas Gredin <t.gredin@gmx.fr>
2019-06-04 11:52:23 +0200

    Create Ajax class in front-end app

---
+++ clpm_client/src/utils/Ajax.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+/**
+ * Class Ajax
+ * Utils to perform Ajax request and fetch/modify data
+ * from distant server.
+ */
+export default class Ajax {
+    constructor(base_url) {
+        this.base_url = base_url
+    }
+
+    get(path) {
+
+    }
+
+    post(path) {
+
+    }
+}
