9468af7ca1bf48501c0a528ca7f2a544de97bbd5
Thomas Gredin <t.gredin@gmx.fr>
2019-06-16 18:11:17 +0200

    Create all file for app pages

---
+++ clpm_client/src/components/elements/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="navbar">
+        <span class="brand">C L P M</span>
+        <div class="user-actions">
+            <span class="username">Bonjour {{ this.username }}</span>
+            <a class="logout-btn" href="" @click.prevent="logout()">Déconnexion</a>
+        </div>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+import axios from '../../services/api'
+
+export default {
+    methods: {
+        logout: function() {
+            let self = this;
+            axios.post('auth/logout/')
+                .then(function(response) {
+                    console.log("qdzdqzd");
+                    self.$router.push({path: '/login'});
+                })
+                .catch(function(error) { 
+                    console.log(error.data) 
+                })
+        }
+    },
+    computed: {
+        username() {
+            return this.$store.state.username;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        grid-area: nav;
+        background-color: darken(#2d2d2d, 3%);
+        color: white;
+
+        .brand {
+            display: block;
+            height: 50px;
+            width: 200px;
+            text-align: center;
+            line-height: 50px;
+            font-weight: 600;
+        }
+
+        .user-actions {
+            display: flex;
+
+            .logout-btn {
+                font-size: 0.7em;
+                display: block;
+                height: 50px;
+                margin: 0 20px 0 0;
+                text-align: center;
+                line-height: 50px;
+                text-decoration: none;
+                color: white;
+            }
+
+            .username {
+                display: block;
+                height: 50px;
+                margin: 0 20px 0 0;
+                text-align: center;
+                line-height: 50px;
+                text-decoration: none;
+                color: white;
+                font-weight: 400;
+            }
+        }
+    }
+</style>
---
+++ clpm_client/src/components/elements/SideNav.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="sidenav">
+        <ul class="menu">
+            <li><router-link to="/">Accueil</router-link></li>
+            <li><router-link to="/account">Compte</router-link></li>
+            <li><router-link to="/wallet">Portefeuille</router-link></li>
+            <li><router-link to="/project">Projets</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .sidenav {
+        grid-area: sidenav;
+        background-color: #2d2d2d;
+        color: white;
+        height: 100%;
+
+        .menu {
+            margin: 50px 0 0 0;
+            list-style: none;
+
+            a {
+                display: block;
+                color: white;
+                text-decoration: none;
+
+
+                height: 33px;
+                line-height: 33px;
+
+                &.router-link-active {
+                    color: #d64937;
+                }
+
+                &:hover {
+                    color: #d64937;
+                }
+            }
+        }
+    }
+</style>
