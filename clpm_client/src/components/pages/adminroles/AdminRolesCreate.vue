35233f329f338768939fc645ca6bdb9f81991d6e
Thomas Gredin <t.gredin@gmx.fr>
2019-06-18 16:18:28 +0200

    Create component for role creation admin

---
+++ clpm_client/src/components/pages/adminroles/AdminRolesCreate.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="admin-roles-create">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .admin-roles-create {
+        padding: 20px;
+        overflow-y: auto;
+    }
+</style>
--- clpm_client/src/components/pages/adminroles/AdminRolesHome.vue
+++ clpm_client/src/components/pages/adminroles/AdminRolesHome.vue
@@ -1,14 +1,41 @@
 <template>
     <div class="admin-roles-home">
         <div class="box">
-            Admin roles home
+            <div class="space-between">
+                <h3>Rôles</h3>
+                <div>
+                    <router-link to="/app/admin" class="button success">+ Créer</router-link> &nbsp;
+                    <router-link to="/app/admin" class="button primary">Retour</router-link>
+                </div>
+            </div>
+
+            <table class="data-table">
+                <tbody>
+                    <tr v-for="role in rolesList" :key="role.id">
+                        <td>{{ role.name }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
 
 <script>
+import axios from '@/services/api'
+
 export default {
-    
+    data() {
+        return {
+            rolesList: {}
+        }
+    },
+
+    mounted() {
+        let self = this;
+        axios.get('api/project/role/list').then(function(response) {
+            self.rolesList = response.data;
+        });
+    }
 }
 </script>
 
