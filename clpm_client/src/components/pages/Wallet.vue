aa16032c8239f21fb736b4eb1efb433f6558d2e7
Romain <romain.huyvaert@gmail.com>
2019-06-19 15:05:13 +0200

    Récupérationd des transactions et affichage

--- clpm_client/src/components/pages/Wallet.vue
+++ clpm_client/src/components/pages/Wallet.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="wallet">
         <div class="box">
-            <h3>Portefeuille</h3>
-            <div class="amount box">
-                <h4>Solde : {{this.userData.balance}} €</h4>
-                <router-link :to="{name : 'app.supply', params: {id: userData.id}}" class="button success">Ajouter des fonds</router-link>
-            </div>
+            <h3>Portefeuille</h3><br>
+            <h4>Solde : {{userData.balance}} €</h4>
+            <router-link :to="{name : 'app.supply', params: {id: userData.id}}" class="button success">Ajouter des fonds</router-link><br>
+            <h4>Dernières transactions :</h4>
+            <table class="data-table">
+                <tbody>
+                    <tr v-for="transaction in userTransactions" :key="transaction.account_id">
+                        <td>Transaction du : {{transaction.created_at}}</td>
+                        <td style="text-align: right">Montant : {{transaction.amount}} €</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
@@ -16,7 +23,8 @@ import axios from '@/services/api'
 export default {
     data() {
         return {
-            userData: {}
+            userData: {},
+            userTransactions: {},
         }
     },
     mounted() {
@@ -25,7 +33,10 @@ export default {
             let userId = response.data.id;
             axios.get('api/account/' + userId + '/details').then(function(response) {
                 self.userData = response.data;
-            })
+            });
+            axios.get('api/account/' + userId + '/transaction').then(function(response) {
+                self.userTransactions = response.data;
+            });
         })
     }
 }
