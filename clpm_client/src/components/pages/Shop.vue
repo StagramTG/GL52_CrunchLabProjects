7cf3eb6f7f41d7a4da90d0b8d137126a22b2b638
Romain <romain.huyvaert@gmail.com>
2019-06-21 02:55:06 +0200

    Affichage des produits dans la boutique

--- clpm_client/src/components/pages/Shop.vue
+++ clpm_client/src/components/pages/Shop.vue
@@ -1,13 +1,44 @@
 <template>
     <div class="shop">
-        Boutique
+        <div class="box">
+            <h3>Boutique</h3>
+            <table class="data-table">
+                <tbody>
+                    <tr v-for="product in products" :key="product.id">
+                        <div class="space-between">
+                            <td>{{product.name}} : {{product.price}} €</td>
+                            <td><a href="" class="button success" @click.prevent="add_cart(product.id)">Ajouter au panier</a></td>
+                        </div>
+                    </tr>
+                </tbody>
+            </table>
+            <a href="" class="button success right" @click.prevent="">Consulter le panier</a>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
+import axios from '@/services/api'
 
+export default {
+    data() {
+        return {
+            products: {},
+            cart: [],
+        }
+    },
+    mounted() {
+        let self = this;
+        axios.get('api/products/list').then(function(response){
+            self.products = response.data;
+        })
+    },
+    methods: {
+        add_cart(id) {
+            this.cart.push(id);
+        }
     }
+}
 </script>
 
 <style lang="scss">
